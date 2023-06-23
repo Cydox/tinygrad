eb18c9c1e97fef812192e567cb85887fd689ad87
Jan Hendrik Farr <github@jfarr.cc>
2023-06-23 14:36:21 +0200

    little more output

--- test/test_dtype.py
+++ test/test_dtype.py
@@ -34,6 +34,7 @@ class TestHalfDtype(unittest.TestCase):
 
   def test_half_to_float(self): _test_cast(Tensor([1,2,3,4], dtype=dtypes.float16), dtypes.float32, [1,2,3,4])
   def test_half_to_int8(self): _test_cast(Tensor([1,2,3,4], dtype=dtypes.float16), dtypes.int8, [1,2,3,4])
+  def test_half_to_int8(self): _test_cast(Tensor([0,2,3,4], dtype=dtypes.float16), dtypes.int8, [1,2,3,4])
   # def test_half_to_int8_1(self): _test_cast(Tensor([1,1,1,1], dtype=dtypes.float16), dtypes.int8, [1,1,1,1])
   # def test_half_to_int8_2(self): _test_cast(Tensor([2,2,2,2], dtype=dtypes.float16), dtypes.int8, [2,2,2,2])
   # def test_half_to_int8_3(self): _test_cast(Tensor([0,0,0,0], dtype=dtypes.float16), dtypes.int8, [0,0,0,0])
