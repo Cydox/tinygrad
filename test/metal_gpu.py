b2531a5f72596215e6548006b392c3dbea9afbb9
Jan Hendrik Farr <github@jfarr.cc>
2023-06-24 16:26:14 -0700

    test

--- test/metal_gpu.py
+++ test/metal_gpu.py
@@ -104,6 +104,9 @@ kernel void E_4(device char* data0, const device half* data1, uint3 gid [[thread
     //simdgroup_barrier(mem_flags::mem_threadgroup);
     data0[0] = val1_0.x + data0[1];
     data0[1] = val1_0.y;
+    if data0[1] != val1_0.y {
+      return;
+    }
     data0[2] = val1_0.z;
     data0[3] = val1_0.w + data0[1];
     threadgroup_barrier(mem_flags::mem_threadgroup);
