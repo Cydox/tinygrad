ebf9e4bb4352a2a028ef63d8a971d5ff7c8395f5
Jan Hendrik Farr <github@jfarr.cc>
2023-06-24 18:34:40 -0700

    test

--- tinygrad/codegen/cstyle.py
+++ tinygrad/codegen/cstyle.py
@@ -148,7 +148,7 @@ def uops_to_cstyle(uops:List[UOp], bufs:List[Union[LocalBuffer,LazyBuffer]], lan
       else:
         casts = {dtypes._float4: ("", f"{lang.float4}(0.0f, 0.0f, 0.0f, 0.0f)"), dtypes.half: ("(half)", "(half)(0.0f)"), dtypes.float: ("(float)", "0.0f")}[newvar.dtype]
         kk(f"{newvar.render(True)} = ({args.valid.render(render_cl)}) ? {casts[0]}({val}) : {casts[1]};")
-      if apply_barrier: kk(lang.barrier)
+      # if apply_barrier: kk(lang.barrier)
     elif uop == UOps.STORE and (vin[0].dtype == dtypes.float or (vin[0].dtype == dtypes._float4 and vin[0].offset is not None)):
       assert not isinstance(bufs[args.i].dtype, ImageDType), "image store must be float4"
       assert args.valid.min == 1, "store must be valid"
