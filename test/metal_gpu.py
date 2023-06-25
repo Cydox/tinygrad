11aa06ee86f615579c2840c088c9fd294a63096d
Jan Hendrik Farr <github@jfarr.cc>
2023-06-24 20:05:55 -0700

    test

--- test/metal_gpu.py
+++ test/metal_gpu.py
@@ -123,10 +123,10 @@ kernel void E_4(device char* data0, const device half* data1, uint3 gid [[thread
     half val1_2 = data1[2];
     half val1_3 = data1[3];
     //threadgroup_barrier(mem_flags::mem_threadgroup);
-    data0[0] = val1_0;
-    data0[1] = val1_1;
-    data0[2] = val1_2;
-    data0[3] = val1_3 + data0[1];
+    data0[0] = static_cast<char>(val1_0);
+    data0[1] = static_cast<char>(val1_1);
+    data0[2] = static_cast<char>(val1_2);
+    data0[3] = static_cast<char>(val1_3);
     threadgroup_barrier(mem_flags::mem_threadgroup);
     if (0 == 0) {
   } /* local */
